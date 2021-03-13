4ea2b84e5477ce2bc9ed9d9aef841f71367370be
AliciaJK <alicia.krolak4@gmail.com>
2021-03-12 21:57:46 -0500

    finished product

--- Develop/script.js
+++ Develop/script.js
@@ -9,31 +9,39 @@ function writePassword() {
   passwordText.value = password;
 
 }
-
+generateBtn.addEventListener("click", writePassword);
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------
-//dont really get what Im going here
-function generatePassword(){}
+//dont really get what Im going heregit git
+function generatePassword(){
 
 
-
+var thePassword = "";
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
 
 // this is the possible selections: 
 
 var alphaCaps = ["A", "B", "C", "D", "E", "F","G", "H", "I", "J", "K", "L","M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X","Y","Z"];  
 var alphaLower =  ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l","m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x","y","z"];
 var number = ["1", "2", "3","4", "5", "6", "7", "8","9"]; 
-var specialChar = ["!", "@", "#", "$", "%","^", "&", "*", "(", ")", ".", ",", ">", "<", "-"]; 
+var specialChar = ["!", "@", "#", "$", "%","^", "&", "*", "(", ")", ".", ">", "<", "-"]; 
 
 
-// need the use to select generate passoword
-// this will be linked to the generateBtn above. 
+var passwordLength = 8
 
+
+ function thePrompts () {
+   passwordLength = prompt("How many characters is your password?")
+
+   if (passwordLength <8 || passwordLength >128)
+    thePrompts();
+ }
+
+
+ thePrompts();
 //----------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // 1 need pop up asking for passowrd length
-var passwordLength = prompt("How many characters is your password?")
+//var passwordLength = prompt("How many characters is your password?")
 
 // 2 ask user if they want numbers  //ok will be true and cancel will be false 
 var userNum = confirm("Do you want numbers?")
@@ -50,45 +58,39 @@ var userLowercase = confirm("Do you want lowercase letters?")
 //---------------------------------------------------------------------------------------------------------------------------------------------------------------------
 // if  user select  A and B and D then concat those & then random select
 
-if (userNum===true && userLowercase===true && userCaps===true && userSpChar===true){
-// then concat all lists 
-passwordArray = userNum.concat(userLowercase, userCaps, userSpChar);
+var passwordArray = []
+
+if (userNum){
+passwordArray = passwordArray.concat(number);
 console.log(passwordArray)
-
-  } else if(userNum===false && userLowercase===true && userCaps===true && userSpChar===true){
-  // then concat true lists 
-  passwordArray = userSpChar.concat(userLowercase, userCaps)
-  console.log(passwordArray)
-
-  } else if(userNum===false && userLowercase===false && userCaps===true && userSpChar===true){
-  // then concat true lists 
-  passwordArray = userSpChar.concat(userCaps);
-  console.log(passwordArray)
-
-  } else if(userNum===false && userLowercase===false && userCaps===false && userSpChar===true){
-  // then concat true lists
-  passwordArray = userSpChar.concat(userLowercase, userCaps);
-  console.log(passwordArray)
-
-  } else if(userNum===false && userLowercase===false && userCaps===false && userSpChar===false){
-  // then end the whole thing
-
-  } else if(userNum===true && userLowercase===false && userCaps===true && userSpChar===true){
-  // then concat true lists
-  passwordArray = userSpChar.concat(userNum, userCaps);
-  console.log(passwordArray)
-}
-//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+} 
   
-//i is the counter (tells how long the passowrd needs to be by taking the user input of length )
-for(var i=0;i < passwordLength;i++)
-{var index = Math.floor(Math.random() * passwordArray.length);
+if (userSpChar){
+  passwordArray = passwordArray.concat(specialChar);
+  console.log(passwordArray)
+  } 
 
-console.log(alphaCaps[index])
-// now randomize the variable you just listed. 
-function shuffle(passwordArray)
+  if (userLowercase){
+    passwordArray = passwordArray.concat(alphaLower);
+    console.log(passwordArray)
+    } 
 
-// print passwork in this box       <button id="generate" class="btn">Generate Password</button>
+    if (userCaps){
+      passwordArray = passwordArray.concat(alphaCaps);
+      console.log(passwordArray)
+      } 
 
 
-// if user presses cancel at any point in time 
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+        
+      //i is the counter (tells how long the passowrd needs to be by taking the user input of length )
+      for(var i=0;i < passwordLength;i++){
+        var thePassword = thePassword + passwordArray[ Math.floor(Math.random() * passwordArray.length)];
+
+      console.log(thePassword)
+      }
+
+
+      //this calls the password we created
+return thePassword;
+}
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # PasswordGeneratorAK
+live site: 
+https://aliciajk.github.io/PasswordGeneratorAK/.
+
+CSS and HTML were already given to us as part of the assignment
+
+# how it works
+1. asks users what length of password they want
+2. checks if the password length is between 8-128 characters
+3. if no, the prompt will return again asking the user for the appropriate length of password
+4. if yes, it prompts the user if they want capital letters, special characters, lowercase letters and numbers. 
+5. the user selects ok for (yes) and cancel for (no)
+6. the program runs through an if statement such as the following: if user selected yes for any of the 4 possible types of characters then take the "password array" which would be empty and concatinate it to the array the users wanted characters. Ex: numbers yes and lowercase yes, then 12345...abcdef etc 
+7. the program then takes a random selection of the new contatinated array of the indicated length of characters 
+8. it returns and print the password visible to the user on screen.
